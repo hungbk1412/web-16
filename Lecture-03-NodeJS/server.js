69ce1f7ec82f5dd6605e6e4a913cfdf4e1aac97b
Nguyen Tien Hung <hung.nt142180@sis.hust.edu.vn>
2018-11-13 10:41:05 +0700

    * Bonus: Gộp /web16 /web15,... vào để xử lý chung trong một cái.

--- Lecture-03-NodeJS/server.js
+++ Lecture-03-NodeJS/server.js
@@ -1,91 +1,43 @@
 const express = require('express');
-let app = express();
+const app = express();
 
 const path = require('path');
 const axios = require('axios');
 
 app.use(express.static("view"));
 
-app.get('/web16', (req, res) => {
+app.get('/:class', (req, res) => {
+    const subLink = req.params.class;
     axios({
-        url: 'https://btvn-web16s.herokuapp.com/web16',
+        url: 'https://btvn-web16s.herokuapp.com/' + subLink,
         method: 'GET'
-    })
-    .then(function(data) {
+    }).then((data) => {
         res.send(data.data);
     })
 })
 
-app.get('/web16', (req, res) => {
-    axios({
-        url: 'https://btvn-web16s.herokuapp.com/web16',
-        method: 'GET'
-    })
-    .then(function(data) {
-        res.send(data.data);
-    })
-})
+// Hàm trên tương đương với tổng các hàm sau gộp lại:
+// app.get('/web16', (req, res) => {
+//     axios({
+//         url: 'https://btvn-web16s.herokuapp.com/web16',
+//         method: 'GET'
+//     })
+//     .then(function(data) {
+//         res.send(data.data);
+//     })
+// })
 
-app.get('/web15', (req, res) => {
-    axios({
-        url: 'https://btvn-web16s.herokuapp.com/web15',
-        method: 'GET'
-    })
-    .then(function(data) {
-        res.send(data.data);
-    })
-})
+// app.get('/web15', (req, res) => {
+//     axios({
+//         url: 'https://btvn-web16s.herokuapp.com/web15',
+//         method: 'GET'
+//     })
+//     .then(function(data) {
+//         res.send(data.data);
+//     })
+// })
 
-app.get('/web14', (req, res) => {
-    axios({
-        url: 'https://btvn-web16s.herokuapp.com/web14',
-        method: 'GET'
-    })
-    .then(function(data) {
-        res.send(data.data);
-    })
-})
-
-app.get('/web13', (req, res) => {
-    axios({
-        url: 'https://btvn-web16s.herokuapp.com/web13',
-        method: 'GET'
-    })
-    .then(function(data) {
-        res.send(data.data);
-    })
-})
-
-app.get('/web12', (req, res) => {
-    axios({
-        url: 'https://btvn-web16s.herokuapp.com/web12',
-        method: 'GET'
-    })
-    .then(function(data) {
-        res.send(data.data);
-    })
-})
-
-app.get('/web11', (req, res) => {
-    axios({
-        url: 'https://btvn-web16s.herokuapp.com/web11',
-        method: 'GET'
-    })
-    .then(function(data) {
-        res.send(data.data);
-    })
-})
-
-app.get('/web10', (req, res) => {
-    axios({
-        url: 'https://btvn-web16s.herokuapp.com/web10',
-        method: 'GET'
-    })
-    .then(function(data) {
-        res.send(data.data);
-    })
-})
-
+//...etc...
 
 app.listen(1412, (err) => {
     if (err) console.log(err);
